801b72778edce1669af64fe19ed6526c7f5dd185
Clément Nicolle <clement.nicolle@student.ecp.fr>
2015-04-11 17:48:35 +0200

    Bag of words and kNN in main

--- Final project/main.py
+++ Final project/main.py
@@ -6,6 +6,16 @@ Main file for the ALTeGraD final assignment
 
 
 from loaddata import load_data
+from bagofwords import tf_idf
+from nearestneighbors import kNN_predict
 
 # load the data and labels
 train_data, train_labels, test_data, test_labels = load_data()
+
+# get tf_idf matrices
+tfidf_train = tf_idf(train_data)
+tfidf_test = tf_idf(test_data)
+
+# Nearest Neighbors
+k = 5
+labels_pred_kNN = kNN_predict(k, tfidf_train, train_labels, tfidf_test)
