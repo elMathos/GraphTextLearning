06c291a5326239735f09c18b432d76044594bb76
Clément Nicolle <clement.nicolle@student.ecp.fr>
2015-04-13 14:14:23 +0200

    Crossval for adaboost

--- Final project/crossvalidation.py
+++ Final project/crossvalidation.py
@@ -4,7 +4,7 @@ Cross-validation on train dataset to find optimal values for parameters
 
 import numpy as np
 from nearestneighbors import kNN_predict
-from classif import SVM_predict, RF_predict
+from classif import SVM_predict, RF_predict, Adaboost_predict
 from evaluation import precision, recall
 
 
@@ -84,7 +84,35 @@ def crossval_RF(n_fold, train_data, train_labels, ntrees_range):
                                               train_labels[(j+1) *
                                               subset_size:]))
             lab_pred = RF_predict(round_train_data, round_train_lab,
-                                   round_test_data, n_estim = ntrees)
+                                   round_test_data, n_estim=ntrees)
+        micro_prec[i] = precision(round_test_lab, lab_pred,
+                                  avg_type='micro')
+        macro_prec[i] = precision(round_test_lab, lab_pred,
+                                  avg_type='macro')
+        macro_rec[i] = recall(round_test_lab, lab_pred,
+                              avg_type='macro')
+
+    return micro_prec, macro_prec, macro_rec
+
+
+def crossval_Adaboost(n_fold, train_data, train_labels, nbound_range):
+    n_nbound = len(nbound_range)
+    micro_prec = np.zeros(n_nbound)
+    macro_prec = np.zeros(n_nbound)
+    macro_rec = np.zeros(n_nbound)
+    subset_size = len(train_data)/n_fold
+    for i in range(n_nbound):
+        nbound = nbound_range[i]
+        for j in range(n_fold):
+            round_test_data = train_data[j*subset_size:(j+1)*subset_size]
+            round_train_data = np.vstack((train_data[:j*subset_size],
+                                          train_data[(j+1)*subset_size:]))
+            round_test_lab = train_labels[i*subset_size:(j+1)*subset_size]
+            round_train_lab = np.concatenate((train_labels[:j*subset_size],
+                                              train_labels[(j+1) *
+                                              subset_size:]))
+            lab_pred = Adaboost_predict(round_train_data, round_train_lab,
+                                        round_test_data, n_estim=nbound)
         micro_prec[i] = precision(round_test_lab, lab_pred,
                                   avg_type='micro')
         macro_prec[i] = precision(round_test_lab, lab_pred,
