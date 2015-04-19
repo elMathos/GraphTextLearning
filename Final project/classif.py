7fd27fcb30848b9c4274c30efc4e3aaf71775dd2
elMathos <mathurin.massias@gmail.com>
2015-04-19 11:19:42 +0200

    Execution time

--- Final project/classif.py
+++ Final project/classif.py
@@ -5,23 +5,30 @@ Bag-of-words classifiers
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
+import time
 
 
 def SVM_predict(train_data, train_labels, test_data,
                 C=1.0, gamma=0.1, verbose=2):
+    start = time.time()
     clf = svm.SVC(C=C, gamma=gamma)
     clf.fit(train_data, train_labels)
+    print "Elapsed time: " + str(time.time() - start)
     return clf.predict(test_data)
 
 
 def RF_predict(train_data, train_labels, test_data, n_estim=10, verbose=2):
+    start = time.time()
     clf = RandomForestClassifier(n_estimators=n_estim)
     clf.fit(train_data, train_labels)
+    print "Elapsed time: " + str(time.time() - start)
     return clf.predict(test_data)
 
 
 def Adaboost_predict(train_data, train_labels, test_data,
                      n_estim=50, verbose=2):
+    start = time.time()
     clf = AdaBoostClassifier(n_estimators=n_estim)
     clf.fit(train_data, train_labels)
+    print "Elapsed time: " + str(time.time() - start)
     return clf.predict(test_data)
